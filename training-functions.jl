3c52fa025f054f4615e730afa7cff26cec566289
John Waczak <john.louis.waczak@gmail.com>
2023-12-06 07:53:46 -0600

    lowerd number of CV folds to 5

--- training-functions.jl
+++ training-functions.jl
@@ -19,7 +19,7 @@ hpo_ranges = Dict("DecisionTree" => Dict("DecisionTreeRegressor" => [(hpname=:mi
                                           ),
                  "XGBoost" => Dict("XGBoostRegressor" => [(hpname=:num_round, lower=50, upper=100),
                                                           (hpname=:eta, lower=0.01, upper=0.5),
-                                                          (hpname=:max_depth, lower=3, upper=6),
+                                                          (hpname=:max_depth, lower=3, upper=7),
                                                           (hpname=:subsample, lower=0.65, upper=1.0),
                                                           (hpname=:colsample_bytree, lower=0.65, upper=1.0),
                                                           (hpname=:lambda, lower=0.0, upper=100.0),  # L2 regularization. Higher makes model more conservative
@@ -29,7 +29,7 @@ hpo_ranges = Dict("DecisionTree" => Dict("DecisionTreeRegressor" => [(hpname=:mi
                   "EvoTrees" => Dict("EvoTreeRegressor" => [(hpname=:nrounds,lower=50, upper=100),
                                                             (hpname=:nbins, lower=64, upper=255),
                                                             (hpname=:eta, lower=0.01, upper=0.5),
-                                                            (hpname=:max_depth, lower=3, upper=6),
+                                                            (hpname=:max_depth, lower=3, upper=7),
                                                             (hpname=:rowsample, lower=0.65, upper=1.0),
                                                             (hpname=:colsample, lower=0.65, upper=1.0),
                                                             (hpname=:lambda, lower=0.0, upper=100.0),
@@ -411,7 +411,7 @@ function train_hpo(
         range=rs,
         tuning=tuning,
         measures=[rmse, mae, rsq],
-        resampling=CV(nfolds=10, rng=rng),
+        resampling=CV(nfolds=6, rng=rng),
         n=nmodels,
         cache=false,
     )
@@ -680,7 +680,7 @@ function train_stack(
                   lgbr=lgbr,
                   nnr=nnr,
                   catr=catr,
-                  resampling=CV(nfolds=10, rng=rng),
+                  resampling=CV(nfolds=6, rng=rng),
                   cache=false,
                   )
 
