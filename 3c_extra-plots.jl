b3b9c17c0e6f63cb99c097b3e3e05970dfc6fab9
John Waczak <john.louis.waczak@gmail.com>
2024-01-03 12:01:45 -0600

    added histogram for total downwelling intensity

--- 3c_extra-plots.jl
+++ 3c_extra-plots.jl
@@ -1,7 +1,5 @@
-using HDF5, CSV, DataFrames, JSON, Tables
-using MLJ, ConformalPrediction
+using CSV, DataFrames, JSON, Tables
 using Random, Statistics
-using ProgressMeter
 using CairoMakie
 using MintsMakieRecipes
 
@@ -43,24 +41,33 @@ names(df_1123)
 Σdownwelling_1209 = df_1209.Σdownwelling
 Σdownwelling_1210 = df_1210.Σdownwelling
 
+nb1, _ = get_n_bins(Σdownwelling_1123)
+nb2, _ = get_n_bins(Σdownwelling_1209)
+nb3, _ = get_n_bins(Σdownwelling_1210)
+
+
 fig = Figure();
-ax = Axis(fig[1,1], xlabel="Total Downwelling Intensity");
-d = density!(ax, Σdownwelling_1123)
-d2 = density!(ax, Σdownwelling_1209)
-d3 = density!(ax, Σdownwelling_1210)
-
-
+ax = Axis(fig[1,1], xlabel="Downwelling Intensity (W/m²)", ylabel="count", title="Downwelling Intensity Variation");
+h1 = hist!(ax, Σdownwelling_1123, bins=nb1, color=(mints_colors[1], 0.7))
+h2 = hist!(ax, Σdownwelling_1209, bins=nb2, color=(mints_colors[2], 0.7))
+h3 = hist!(ax, Σdownwelling_1210, bins=nb3, color=(mints_colors[3], 0.7))
+leg = axislegend(ax, [h1, h2, h3], ["11-23", "12-09", "12-10"], position=:lt)
 fig
+
+save("./paper/figures/results/downwelling-hist.png", fig)
+save("./paper/figures/results/downwelling-hist.pdf", fig)
+
+
 
 Σdownwelling_1123
 Σdownwelling_1209
 Σdownwelling_1210
 
-h5_test_path = "/Users/johnwaczak/data/robot-team/processed/hsi/11-23/Scotty_1/Scotty_1-1.h5"
-@assert isfile(h5_test_path)
+# h5_test_path = "/Users/johnwaczak/data/robot-team/processed/hsi/11-23/Scotty_1/Scotty_1-1.h5"
+# @assert isfile(h5_test_path)
 
-h = h5open(h5_test_path, "r")
-Data = h["data-Δx_0.1/Data"][:,:,:];
-varnames = h["data-Δx_0.1/varnames"][:];
+# h = h5open(h5_test_path, "r")
+# Data = h["data-Δx_0.1/Data"][:,:,:];
+# varnames = h["data-Δx_0.1/varnames"][:];
 
-Data[end-1,:,:]
+# Data[end-1,:,:]
